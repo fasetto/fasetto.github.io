b16162fc362d7a7dec43e63e033950f2a6e11c1f
SERKAN BIRCAN <serkanbircan21@outlook.com>
2018-09-20 13:56:16 +0300

    added image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 
 var csso = require('gulp-csso');
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 var runSequence = require('run-sequence');
 var uglify = require('gulp-uglify');
 
-gulp.task('styles', function () {
+gulp.task('styles', () => {
     return gulp.src('./_site/assets/css/**/*.css')
         .pipe(csso())
         .pipe(gulp.dest('./_site/assets/css'))
 });
 
-gulp.task('scripts', function () {
+gulp.task('scripts', () => {
     return gulp.src('./_site/assets/js/src/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./_site/assets/js/src/'))
 });
 
-gulp.task('pages', function () {
+gulp.task('pages', () => {
     return gulp.src(['./_site/**/*.html'])
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -26,12 +27,19 @@ gulp.task('pages', function () {
         .pipe(gulp.dest('./_site/'));
 });
 
+gulp.task('images', () => {
+    return gulp.src(['./_site/assets/images/**/*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./_site/assets/images/'));
+});
+
 
 // Gulp task to minify all files
-gulp.task('default', function () {
+gulp.task('default', () => {
   runSequence(
     'styles',
     'scripts',
-    'pages'
+    'pages',
+    'images'
   );
 });
